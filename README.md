53989b4069ef8e5b1fda4406cc1c85d2bc18de2f
Clementine <90754999+krishnamodepalli@users.noreply.github.com>
2024-03-20 22:31:17 +0530

    feat: Add multi-browser support for the automation(chrome & edge)
    Both Chrome and Microsoft Edge are compatible for our automation process
    and can now be used without any issues, with a change of single variable
    in the script.

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ developed for making the process of earning bing reward points easier and
 quicker.
 
 > Disclaimer: This project is still under-developed and works well with the 
-> Edge browser only. Will be definitely adding other browser support soon.
+> Edge browser only. Will be definitely adding other browser support soon.  
+> ***Note**: This project is safe to use and is completely working well.*
 
-***Note**: This project is safe to use and is completely working well.*
+## TODO:
+- [ ] Convert this python script into a fully functioning console automation 
+  script
+- [ ] Enable customization of configurations with local user level config files
+- [ ] Add command-line arguments and flags for run-time configuration settings
+- [ ] Adding profiles for the configurations and settings, for more than one 
+  use-cases and more than one bing accounts
+- [ ] Add clear documentation for the project
--- main.py
+++ main.py
@@ -1,43 +1,92 @@
+import os
 import random
 import time
 
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options as EdgeOptions
-# from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-from Searcher import SearchGenerator
-
-options = EdgeOptions()
-# options = ChromeOptions()
-options.add_experimental_option("detach", True)
-# options.add_experimental_option("debuggerAddress", "localhost:8989")
-# options.add_argument("user-data-dir=C:\\Users\\krish\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 2")
-
-# starting the driver
-driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
-# driver = webdriver.Chrome(options=options)
-driver.maximize_window()
+from SearchGenerator import SearchGenerator
 
 
-def make_browser_search(query):
+class BrowserCodes:
+    CHROME = 1
+    BRAVE = 2
+    EDGE = 3
+
+    def get_browser(self, code) -> str:
+        if code == 1:    res = 'chrome'
+        elif code == 2:  res = 'brave'
+        elif code == 3:  res = 'edge'
+        else:
+            raise Exception(f'Unknown code {code} parsed. Please correct the '
+                            f'code you sent')
+        return res
+
+
+# Kindly, please change the below configurations for your use-case and browsers
+app_configuration = {
+    'browser': 'chrome',
+    'port': 8989,
+    'chrome-profile-dir': r'C:\Users\krish\Apps\chrome\profile',
+    'search-count': 10
+}
+
+driver = None
+
+
+# To open the browser and then open the bing search engine
+def set_browser_ready(browser: int) -> None:
+    """Open the specified browser and opens the bing website."""
+    # open the browser with the remote debugging mode and then attach to browser
+
+    global driver
+    if browser in [1, 2]:           # chrome or brave
+        # chrome and brave are well suited with debugging modes for our project
+        options = ChromeOptions()
+        # starting the browser
+        os.system(
+            f'start chrome --remote-debugging-port={app_configuration["port"]} \
+            --user-data-dir="{app_configuration["chrome-profile-dir"]}"')
+        # attaching the driver to the browser
+        options.add_experimental_option("debuggerAddress", "localhost:8989")
+        driver = webdriver.Chrome(options=options)
+    elif browser == 3:              # edge
+        options = EdgeOptions()
+        # not closing the browser after completion of the task
+        options.add_experimental_option("detach", True)
+        driver = webdriver.Edge(options=options)
+
+    driver.maximize_window()
     driver.get('https://www.bing.com/')
+
+
+def make_browser_search(query: str) -> None:
+    """
+    While the driver is still on the bing website, this method will go to the
+    search bar and will search for the query given as an argument
+    :param query: The query or the search to be searched in the bing website
+    :return: None
+    """
+    # TODO 2: Make sure the driver is ready with the bing website
     time.sleep(3)
     search_box = driver.find_element(By.ID, 'sb_form_q')
+    # if the search-box is not clear, clear it.
+    search_box.clear()
     search_box.send_keys(query, Keys.ENTER)
 
 
 # TODO 1. Add CLI arguments to pass the no. of searches and also specifying the browser to use
 if __name__ == '__main__':
-    searcher = SearchGenerator()
+    generator = SearchGenerator()
+    set_browser_ready(BrowserCodes.CHROME)
     i = 0
-    for headline in searcher.generate_searches():
+    for headline in generator.generate_searches(
+            app_configuration['search-count']):
         i += 1
         search = " ".join(headline.split()[:random.randint(4, 7)])
         make_browser_search(search)
         time.sleep(2)
-        print(f"\rNo of searches done : {i}", end="", sep="")
-
+        print(f"\rNo of searches done : {i} ✔", end="", sep="")
